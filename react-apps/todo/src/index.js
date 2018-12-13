584f8554e65c9f35ad38ff1bfb2dd73e8bee8d45
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-12-14 01:59:39 +0300

    Modify structure

--- react-apps/todo/src/index.js
+++ react-apps/todo/src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';    // подключаем библитеку react
-import ReactDOM from 'react-dom';    // подключаем библитеку react-dom
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header'
-import TodoList from './components/todo-list'
-import SearchPanel from './components/search-panel'
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
 
+import './index.css';
 
 const App = () => {
 
@@ -15,12 +17,17 @@ const App = () => {
   ];
 
   return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList todos={todoData}/>
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} />
     </div>
-  )
+  );
 };
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />,
+  document.getElementById('root'));
