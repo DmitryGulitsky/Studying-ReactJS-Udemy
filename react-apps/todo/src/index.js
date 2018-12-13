ebd85c1ab0cbfa2a903836226c7b10a982539737
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-12-13 20:03:26 +0300

    Clear /todo directory. Create React components

--- react-apps/todo/src/index.js
+++ react-apps/todo/src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react';    // подключаем библитеку react
+import ReactDOM from 'react-dom';    // подключаем библитеку react-dom
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  )
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const AppHeader = () => {
+  return (
+    <h1>My Todo list</h1>
+  )
+};
+
+const SearchPanel = () => {
+  return <input type="text" placeholder="search" />
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  )
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
