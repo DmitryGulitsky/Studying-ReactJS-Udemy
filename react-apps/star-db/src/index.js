4c301ddca9d384d83a31c38fed573502a2e03df6
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-12-15 16:03:15 +0300

    Transforming api data

--- react-apps/star-db/src/index.js
+++ react-apps/star-db/src/index.js
@@ -1,22 +1,51 @@
 //import React from 'react';
 //import ReactDOM from 'react-dom';
 
-const getResource = async (url) => {
-  const res = await fetch(url);   // говорит, что будет ждать, пока результат промиса не станет доступным. вызов вернет promise
+class SwapiService {    // асинхронный источник данных
+  async getResource(url) {
+    const res = await fetch(url);   // говорит, что будет ждать, пока результат промиса не станет доступным. вызов вернет promise
 
-  if (!res.ok) {
-    throw new Error(`Could not fetch ${url}` +
-    `, recieved ${res.status}`)
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}` +
+        `, recieved ${res.status}`)
+    }
+
+    const  body = await res.json();   // будет ждать, пока не получит тело. вызов вернет promise
+    return body;
   }
 
-  const  body = await res.json();   // будет ждать, пока не получит тело. вызов вернет promise
-  return body;
-};
+  async getAllPeople() {
+    const res = await this.getResource(`https://swapi.co/api/people/`);
+    return res.results;   // возвращаем поле results
+  }
 
-getResource('https://swapi.co/api/people/1dsffdd/')
-  .then((body) => {
-    console.log(body);
-  })
-  .catch((err) => {
-    console.error('Could not fetch', err);
-  });
+  getPerson(id) {
+    return this.getResource(`https://swapi.co/api/people/${id}`)
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource(`https://swapi.co/api/planets/`);
+    return res.results;   // возвращаем поле results
+  }
+
+  getPlanet(id) {
+    return this.getResource(`https://swapi.co/api/planets/${id}`)
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource(`https://swapi.co/api/starships/`);
+    return res.results;   // возвращаем поле results
+  }
+
+  getStarship(id) {
+    return this.getResource(`https://swapi.co/api/starships/${id}`)
+  }
+}
+
+const swapi = new SwapiService();
+
+swapi.getPerson(3).then((p) => {
+
+    console.log(p.name)
+
+});
