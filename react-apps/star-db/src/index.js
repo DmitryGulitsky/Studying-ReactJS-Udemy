2d39e65cbf98e18a8676fbc5fa0a025101187a2c
Dmitry Gulitsky <mrdmzver@gmail.com>
2018-12-15 15:38:52 +0300

    Add getResource async await function

--- react-apps/star-db/src/index.js
+++ react-apps/star-db/src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const getResource = async (url) => {
+  const res = await fetch(url);   // говорит, что будет ждать, пока результат промиса не станет доступным. вызов вернет promise
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}` +
+    `, recieved ${res.status}`)
+  }
+
+  const  body = await res.json();   // будет ждать, пока не получит тело. вызов вернет promise
+  return body;
+};
+
+getResource('https://swapi.co/api/people/1dsffdd/')
+  .then((body) => {
+    console.log(body);
+  })
+  .catch((err) => {
+    console.error('Could not fetch', err);
+  });
